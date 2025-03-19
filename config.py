410dda69b35927406a579f9350cbc63e9069b3ca
Javier-003 <javiertoledo455@gmail.com>
2025-03-19 16:54:17 -0600

    updatw

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Config:
-    MONGO_URI = os.getenv('MONGO_URI,"mongodb+srv://equipo:YCPoxTsZv3Lr6N95@cluster0.kigg6os.mongodb.net/?retryWrites=true&w=majority"')
+    MONGO_URI = os.getenv('MONGO_URI',"mongodb+srv://equipo:YCPoxTsZv3Lr6N95@cluster0.kigg6os.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = os.getenv('DATABASE_NAME')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SESSION_COOKIE_NAME = os.getenv('SESSION_COOKIE_NAME')
