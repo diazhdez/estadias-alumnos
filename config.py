5747b0e6c022106b5466b763691fc5ff7b9c434c
Javier-003 <javiertoledo455@gmail.com>
2025-03-19 16:59:07 -0600

    updatww

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 class Config:
     MONGO_URI = os.getenv('MONGO_URI',"mongodb+srv://equipo:YCPoxTsZv3Lr6N95@cluster0.kigg6os.mongodb.net/?retryWrites=true&w=majority")
-    DATABASE_NAME = os.getenv('DATABASE_NAME')
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    DATABASE_NAME = os.getenv('DATABASE_NAME'"Universidad_Estadias")
+    SECRET_KEY = os.getenv('SECRET_KEY'"M0i1Xc$GfPw3Yz@2SbQ9lKpA5rJhDtE7")
     SESSION_COOKIE_NAME = os.getenv('SESSION_COOKIE_NAME')
 
 
