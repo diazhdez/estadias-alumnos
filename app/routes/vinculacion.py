f77b9775451cf4a78bd1448f7d584213bfa17f17
Javier-003 <javiertoledo455@gmail.com>
2025-03-20 10:47:23 -0600

    vinculacion

--- app/routes/vinculacion.py
+++ app/routes/vinculacion.py
@@ -53,7 +53,7 @@ def Home():
                 # Agregar a la lista de resultados
                 alumnos_con_progreso.append(alumno)
 
-            return render_template("vinculacion/alumnos.html", alumnos=alumnos_con_progreso, periodos=periodos, administrador=admin)
+            return render_template("Vinculacion/alumnos.html", alumnos=alumnos_con_progreso, periodos=periodos, administrador=admin)
         else:
             flash('Acceso denegado: No eres un administrador.', 'danger')
             return redirect(url_for('main.index'))
