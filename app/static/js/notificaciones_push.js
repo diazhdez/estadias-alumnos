074558d135e34773d07414aa02d8d6eef8b7977e
Javier-003 <javiertoledo455@mail.com>
2025-10-06 11:20:55 -0600

    a

--- app/static/js/notificaciones_push.js
+++ app/static/js/notificaciones_push.js
@@ -1,11 +1,8 @@
 const VAPID_PUBLIC_KEY = "BCzzEJZv7f86NFxKGVrExCfqYgQ0ld0-nxTZ9WAsRhbdekEC_Zk8o_fzGS4PlGFJyNlutmqdurS4T4Jobsh0yPk";
 
-
 function urlBase64ToUint8Array(base64String) {
     const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/');
+    const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
     const rawData = window.atob(base64);
     return Uint8Array.from([...rawData].map(char => char.charCodeAt(0)));
 }
